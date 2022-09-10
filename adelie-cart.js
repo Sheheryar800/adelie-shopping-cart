fbf8986a2046a8386a660a2f6205a3d9948c88a9
Sheheryar800 <sheheryarchh55@gmail.com>
2022-09-10 21:55:41 +0500

    working on the testing

--- adelie-cart.js
+++ adelie-cart.js
@@ -1,2 +1,21 @@
-var adelieCart = true
-$('#adelie').html("YAY IT WORKED!!!!!!!!!!")
+class AdelieCart {
+    constructor({marginTop='100px', color='#ce2d30', secretKey="a"}) {
+        this.marginTop = marginTop;
+        this.color = color;
+        this.secretKey = secretKey
+    }
+
+    init(){
+        $('#adelie').html('Class is wokring fine!!!')
+        console.log(this.marginTop, this.color, this.secretKey)
+    }
+    update () {
+        if (this.secretKey) {
+            this.init()
+        }else{
+            console.log("Please provide the secretKey")
+        }
+    }
+}
+
+new AdelieCart({})
